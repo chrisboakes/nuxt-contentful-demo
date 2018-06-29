bb6f8e2a7346896764d1e60661196a4f04ffb5e4
Chris Boakes <chris@chrisboakes.com>
2018-06-29 10:37:31 +0100

    Corrected env example

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -3,5 +3,5 @@ const contentful = require('contentful')
 
 module.exports = contentful.createClient({
     space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_CDA_ACCESS_TOKEN
+    accessToken: process.env.CTF_CD_ACCESS_TOKEN
 })
