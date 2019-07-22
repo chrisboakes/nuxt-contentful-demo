7148dcae0b6f997c7e6282feb6c99fd34fa336c9
Chris Boakes <chris@chrisboakes.com>
2019-07-22 21:00:05 +0100

    Wrapped Contentful instantiation in conditional to solve GitHub issue #1

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const contentful = require('contentful')
 
-module.exports = contentful.createClient({
-    space: process.env.CTF_SPACE_ID,
-    accessToken: process.env.CTF_CD_ACCESS_TOKEN
-})
+if (process.env.CTF_SPACE_ID) {
+    module.exports = contentful.createClient({
+        space: process.env.CTF_SPACE_ID,
+        accessToken: process.env.CTF_CD_ACCESS_TOKEN
+    })
+}
